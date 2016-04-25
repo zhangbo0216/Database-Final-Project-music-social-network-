bc3d586e5113fd7b23de41be8d06b918a0294309
zhangbo0216 <zhangbo0216@hotmail.com>
2016-04-25 13:32:16 -0400

    sign_in and sign up

---
+++ check_my_post.php
@@ -0,0 +1,23 @@
+<html>
+	<body>
+		<?php
+			$username=$_POST["username"];
+			$password=$_POST["password"];
+			$user = 'root';
+			$lpassword = 'root';
+			$db = 'music_social';
+			$host = 'localhost';
+			$port = 3306;
+
+			$link = mysqli_init();
+			$success = mysqli_real_connect(
+   			$link, 
+   			$host, 
+   			$user, 
+   			$lpassword, 
+   			$db,
+   			$port);
+   			$result
+   		?>
+	</body>
+</html> 
---
+++ main.php
@@ -0,0 +1,42 @@
+<html>
+	<body>
+		<?php
+			$username=$_POST["username"];
+			$password=$_POST["password"];
+			$user = 'root';
+			$lpassword = 'root';
+			$db = 'music_social';
+			$host = 'localhost';
+			$port = 3306;
+
+			$link = mysqli_init();
+			$success = mysqli_real_connect(
+   			$link, 
+   			$host, 
+   			$user, 
+   			$lpassword, 
+   			$db,
+   			$port);
+   			$result = mysqli_query($link,"select * from fans where username='$username' and password='$password'");
+   			
+   			$c_password = mysqli_fetch_array($result);
+   			if ($c_password['username']!=$username)
+   				echo "You enter the Wrong username or password, please check again.";
+   			else
+   			{
+   				echo "
+   				<form action='edit_profile.php' method='post'>
+  					<button type='submit' name='username' value=$username>Edit My Profile</button>
+  				</form>
+   				<form action='new_post.php' method='post'>
+  					<button type='submit' name='username' value=$username>New Post</button>
+  				</form>
+  				<form action='check_my_post.php' method='post'>
+  					<button type='submit' name='username' value=$username>Check My Post</button>
+  				</form>
+  				
+  				";
+   			}
+		?>
+	</body>
+</html> 
